0822e1e8ad6dc6fc9e4cb14843e2c21f58b0e641
Ameee <quyhau258@gmail.com>
2018-02-27 10:49:34 +0700

    Add project files.

---
+++ QuanLySinhVien/Untill.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Data;
+
+namespace QuanLySinhVien
+{
+    class Untill
+    {
+        public static SqlConnection con;
+        public static SqlCommand cmd;
+        public static SqlDataAdapter da;
+        public SqlConnection OpenDB()
+        {
+            con = new SqlConnection("Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            return con;
+        }
+        public static void OpenCon()
+        {
+            string sql = "Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True";
+            try
+            {
+                con = new SqlConnection(sql);
+                con.Open();
+            }
+            catch(SqlException Ex)
+            { }
+        }
+        public static void CloseCon()
+        {
+            con.Close();
+            con.Dispose();
+            con = null;
+        }
+        public static DataTable TaoBang(string sql)
+        {
+            cmd = new SqlCommand(sql, con);
+            da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            DataTable ds = new DataTable();
+            da.Fill(ds);
+            da.Dispose();
+            cmd.Dispose();
+            return ds;
+
+        }
+        public static void ThaoTac(string sql)
+        {
+            cmd = new SqlCommand(sql, con);
+            cmd.ExecuteNonQuery();
+        }
+    }
+}
