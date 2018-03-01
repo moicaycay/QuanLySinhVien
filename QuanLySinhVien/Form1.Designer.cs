75317fa0b4cde2f54a9c34ff0bdababca8cc84ae
Ameee <quyhau258@gmail.com>
2018-03-01 23:19:26 +0700

    BaiTap2
    TabControl

--- QuanLySinhVien/Form1.Designer.cs
+++ QuanLySinhVien/Form1.Designer.cs
@@ -33,52 +33,249 @@
             this.sINHVIENBindingSource1 = new System.Windows.Forms.BindingSource(this.components);
             this.sINHVIENBindingSource = new System.Windows.Forms.BindingSource(this.components);
             this.sINHVIENTableAdapter = new QuanLySinhVien.QuanLySinhVienDataSetTableAdapters.SINHVIENTableAdapter();
-            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.cbbBang1 = new System.Windows.Forms.ComboBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.tabControl1 = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tenbang = new System.Windows.Forms.Label();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.dataGridView2 = new System.Windows.Forms.DataGridView();
+            this.cbbKhoa1 = new System.Windows.Forms.ComboBox();
+            this.tabPage3 = new System.Windows.Forms.TabPage();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView3 = new System.Windows.Forms.DataGridView();
+            this.cbbLop1 = new System.Windows.Forms.ComboBox();
+            this.cbbKhoa2 = new System.Windows.Forms.ComboBox();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sINHVIENBindingSource1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.sINHVIENBindingSource)).BeginInit();
+            this.tabControl1.SuspendLayout();
+            this.tabPage1.SuspendLayout();
+            this.tabPage2.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).BeginInit();
+            this.tabPage3.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).BeginInit();
             this.SuspendLayout();
             // 
             // dataGridView1
             // 
             this.dataGridView1.AllowUserToOrderColumns = true;
-            this.dataGridView1.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
             this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dataGridView1.Location = new System.Drawing.Point(32, 99);
+            this.dataGridView1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView1.Location = new System.Drawing.Point(3, 46);
             this.dataGridView1.Name = "dataGridView1";
-            this.dataGridView1.Size = new System.Drawing.Size(240, 150);
+            this.dataGridView1.Size = new System.Drawing.Size(536, 200);
             this.dataGridView1.TabIndex = 0;
             // 
             // sINHVIENTableAdapter
             // 
             this.sINHVIENTableAdapter.ClearBeforeFill = true;
             // 
-            // comboBox1
+            // cbbBang1
             // 
-            this.comboBox1.FormattingEnabled = true;
-            this.comboBox1.Items.AddRange(new object[] {
+            this.cbbBang1.FormattingEnabled = true;
+            this.cbbBang1.Items.AddRange(new object[] {
             "KHOA",
             "LỚP",
             "SINH VIÊN"});
-            this.comboBox1.Location = new System.Drawing.Point(32, 64);
-            this.comboBox1.Name = "comboBox1";
-            this.comboBox1.Size = new System.Drawing.Size(121, 21);
-            this.comboBox1.TabIndex = 1;
+            this.cbbBang1.Location = new System.Drawing.Point(109, 6);
+            this.cbbBang1.Name = "cbbBang1";
+            this.cbbBang1.Size = new System.Drawing.Size(121, 21);
+            this.cbbBang1.TabIndex = 1;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(254, 6);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 2;
+            this.button1.Text = "Hiển thị";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // tabControl1
+            // 
+            this.tabControl1.Controls.Add(this.tabPage1);
+            this.tabControl1.Controls.Add(this.tabPage2);
+            this.tabControl1.Controls.Add(this.tabPage3);
+            this.tabControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.tabControl1.Location = new System.Drawing.Point(0, 0);
+            this.tabControl1.Name = "tabControl1";
+            this.tabControl1.SelectedIndex = 0;
+            this.tabControl1.Size = new System.Drawing.Size(550, 275);
+            this.tabControl1.TabIndex = 3;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Controls.Add(this.tenbang);
+            this.tabPage1.Controls.Add(this.cbbBang1);
+            this.tabPage1.Controls.Add(this.button1);
+            this.tabPage1.Controls.Add(this.dataGridView1);
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "tabPage1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(542, 249);
+            this.tabPage1.TabIndex = 0;
+            this.tabPage1.Text = "Khoa";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tenbang
+            // 
+            this.tenbang.AutoSize = true;
+            this.tenbang.Location = new System.Drawing.Point(40, 9);
+            this.tenbang.Name = "tenbang";
+            this.tenbang.Size = new System.Drawing.Size(40, 13);
+            this.tenbang.TabIndex = 3;
+            this.tenbang.Text = "KHOA:";
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Controls.Add(this.label1);
+            this.tabPage2.Controls.Add(this.button3);
+            this.tabPage2.Controls.Add(this.dataGridView2);
+            this.tabPage2.Controls.Add(this.cbbKhoa1);
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "tabPage2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(542, 249);
+            this.tabPage2.TabIndex = 1;
+            this.tabPage2.Text = "Lớp";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(24, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(40, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "KHOA:";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(313, 3);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 2;
+            this.button3.Text = "Ok";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // dataGridView2
+            // 
+            this.dataGridView2.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView2.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView2.Location = new System.Drawing.Point(3, 54);
+            this.dataGridView2.Name = "dataGridView2";
+            this.dataGridView2.Size = new System.Drawing.Size(536, 192);
+            this.dataGridView2.TabIndex = 1;
+            // 
+            // cbbKhoa1
+            // 
+            this.cbbKhoa1.FormattingEnabled = true;
+            this.cbbKhoa1.Location = new System.Drawing.Point(68, 6);
+            this.cbbKhoa1.Name = "cbbKhoa1";
+            this.cbbKhoa1.Size = new System.Drawing.Size(121, 21);
+            this.cbbKhoa1.TabIndex = 0;
+            // 
+            // tabPage3
+            // 
+            this.tabPage3.Controls.Add(this.label3);
+            this.tabPage3.Controls.Add(this.label2);
+            this.tabPage3.Controls.Add(this.button2);
+            this.tabPage3.Controls.Add(this.dataGridView3);
+            this.tabPage3.Controls.Add(this.cbbLop1);
+            this.tabPage3.Controls.Add(this.cbbKhoa2);
+            this.tabPage3.Location = new System.Drawing.Point(4, 22);
+            this.tabPage3.Name = "tabPage3";
+            this.tabPage3.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage3.Size = new System.Drawing.Size(542, 249);
+            this.tabPage3.TabIndex = 2;
+            this.tabPage3.Text = "Sinh Viên";
+            this.tabPage3.UseVisualStyleBackColor = true;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(213, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(31, 13);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "LỚP:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(8, 22);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(40, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "KHOA:";
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(407, 17);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 3;
+            this.button2.Text = "OK";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // dataGridView3
+            // 
+            this.dataGridView3.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView3.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView3.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.dataGridView3.Location = new System.Drawing.Point(3, 87);
+            this.dataGridView3.Name = "dataGridView3";
+            this.dataGridView3.Size = new System.Drawing.Size(536, 159);
+            this.dataGridView3.TabIndex = 2;
+            // 
+            // cbbLop1
+            // 
+            this.cbbLop1.FormattingEnabled = true;
+            this.cbbLop1.Location = new System.Drawing.Point(247, 19);
+            this.cbbLop1.Name = "cbbLop1";
+            this.cbbLop1.Size = new System.Drawing.Size(121, 21);
+            this.cbbLop1.TabIndex = 1;
+            // 
+            // cbbKhoa2
+            // 
+            this.cbbKhoa2.FormattingEnabled = true;
+            this.cbbKhoa2.Location = new System.Drawing.Point(60, 19);
+            this.cbbKhoa2.Name = "cbbKhoa2";
+            this.cbbKhoa2.Size = new System.Drawing.Size(121, 21);
+            this.cbbKhoa2.TabIndex = 0;
+            this.cbbKhoa2.SelectedIndexChanged += new System.EventHandler(this.cbbKhoa2_SelectedIndexChanged);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(284, 261);
-            this.Controls.Add(this.comboBox1);
-            this.Controls.Add(this.dataGridView1);
+            this.ClientSize = new System.Drawing.Size(550, 275);
+            this.Controls.Add(this.tabControl1);
             this.Name = "Form1";
             this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.sINHVIENBindingSource1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.sINHVIENBindingSource)).EndInit();
+            this.tabControl1.ResumeLayout(false);
+            this.tabPage1.ResumeLayout(false);
+            this.tabPage1.PerformLayout();
+            this.tabPage2.ResumeLayout(false);
+            this.tabPage2.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView2)).EndInit();
+            this.tabPage3.ResumeLayout(false);
+            this.tabPage3.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView3)).EndInit();
             this.ResumeLayout(false);
 
         }
@@ -88,7 +285,23 @@
         private System.Windows.Forms.BindingSource sINHVIENBindingSource;
         private QuanLySinhVienDataSetTableAdapters.SINHVIENTableAdapter sINHVIENTableAdapter;
         private System.Windows.Forms.BindingSource sINHVIENBindingSource1;
-        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.ComboBox cbbBang1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TabControl tabControl1;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+        private System.Windows.Forms.TabPage tabPage3;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.DataGridView dataGridView2;
+        private System.Windows.Forms.ComboBox cbbKhoa1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView3;
+        private System.Windows.Forms.ComboBox cbbLop1;
+        private System.Windows.Forms.ComboBox cbbKhoa2;
+        private System.Windows.Forms.Label tenbang;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label2;
     }
 }
 
--- QuanLySinhVien/Form1.cs
+++ QuanLySinhVien/Form1.cs
@@ -1,14 +1,7 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
 using System.Data.SqlClient;
-using System.Data;
 
 namespace QuanLySinhVien
 {
@@ -16,8 +9,109 @@ namespace QuanLySinhVien
     {
         public Form1()
         {
-            Untill.OpenCon();
-            dataGridView1.DataSource = Untill.TaoBang("select * from SINHVIEN");
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            // string a = null;
+            //  int b = cbbBang1.SelectedIndex;
+            // switch(b)
+            // {
+            //     case 0: a = "KHOA"; break;
+            //     case 1: a = "LOP"; break;
+            //     case 2: a = "SINHVIEN"; break;
+            // }
+            // string sql = "select * from " + a;
+            //  SqlConnection con = new SqlConnection("Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            //  con.Open();                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                               
+            //  SqlCommand cmd = new SqlCommand(sql, con);
+            //  DataTable dt = new DataTable();
+            //  SqlDataAdapter da = new SqlDataAdapter();
+            //  da.SelectCommand = cmd;
+            //  da.Fill(dt);
+            //  dataGridView1.DataSource = dt;
+            //  con.Close();
+            string a = cbbBang1.SelectedValue.ToString();
+            string sql = "select * from KHOA where MaKhoa = " + "'" + a + "'";
+            SqlConnection con = new SqlConnection("Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            dataGridView1.DataSource = dt;
+            con.Close();
+        }
+        
+        private void button3_Click(object sender, EventArgs e)
+        {
+            string a = cbbKhoa1.SelectedValue.ToString();
+            string sql = "select * from LOP where MaKhoa = " + "'"+a+ "'";
+            SqlConnection con = new SqlConnection("Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            dataGridView2.DataSource = dt;
+            con.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            string sql = "select * from KHOA";
+            SqlConnection con = new SqlConnection("Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            cbbKhoa1.DataSource = dt;
+            cbbKhoa1.DisplayMember = "TenKhoa";
+            cbbKhoa1.ValueMember = "MaKhoa";
+            cbbKhoa2.DataSource = dt;
+            cbbKhoa2.DisplayMember = "TenKhoa";
+            cbbKhoa2.ValueMember = "MaKhoa";
+            cbbBang1.DataSource = dt;
+            cbbBang1.DisplayMember = "TenKhoa";
+            cbbBang1.ValueMember = "MaKhoa";
+            con.Close();
+        }
+
+        private void cbbKhoa2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            string a = cbbKhoa2.SelectedValue.ToString();
+            string sql = "select * from LOP where MaKhoa = " + "'" + a + "'";
+            SqlConnection con = new SqlConnection("Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            cbbLop1.DataSource = dt;
+            cbbLop1.DisplayMember = "TenLop";
+            cbbLop1.ValueMember = "MaLop";
+            con.Close();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            string a = cbbLop1.SelectedValue.ToString();
+            string sql = "select * from SINHVIEN where MaLop = " + "'" + a + "'";
+            SqlConnection con = new SqlConnection("Data Source=AME\\SQLEXPRESS;Initial Catalog=QuanLySinhVien;Integrated Security=True");
+            con.Open();
+            SqlCommand cmd = new SqlCommand(sql, con);
+            DataTable dt = new DataTable();
+            SqlDataAdapter da = new SqlDataAdapter();
+            da.SelectCommand = cmd;
+            da.Fill(dt);
+            dataGridView3.DataSource = dt;
+            con.Close();
         }
     }
 }
